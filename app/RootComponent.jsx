1085331288fe4e022ce34377bbb040e820e210d0
DrPengPeng <58268636@qq.com>
2016-01-05 02:56:27 +0800

    一切来的太突然了。。。完成了导航条的初步设计；完成了边栏导航的初步设计；完成了datagrid的初步设计；

--- app/RootComponent.jsx
+++ app/RootComponent.jsx
@@ -2,20 +2,8 @@ import React from 'react-native';
 import {connect} from 'react-redux';
 import * as actions from './actions/actionsCreators.jsx';
 
-const MK = require('react-native-material-kit');
-// import { Icon, TabBarIOS, Spinner} from 'react-native-icons/index.ios';
-var {Icon} = require('react-native-icons/index.ios');
-const {
-  MKCardStyles,
-  MKButton
-} = MK;
-const ColoredRaisedButton = MKButton.coloredButton()
-  .withText('BUTTON')
-  .withOnPress(() => {
-    console.log("Hi, it's a colored button!");
-  })
-  .build();
-
+import TodayContainer from './containers/TodayContainer.jsx';
+var Drawer = require('react-native-drawer');
 
 const {
   Navigator,
@@ -26,176 +14,101 @@ const {
   TouchableOpacity
 } = React;
 
-
-const SecondPageComponent = React.createClass({
-    getInitialState: function() {
-        return {};
-    },
-    componentDidMount: function() {
-    },
-    _pressButton: function() {
-        const { navigator } = this.props;
-        if(navigator) {
-            //很熟悉吧，入栈出栈~ 把当前的页面pop掉，这里就返回到了上一个页面:irstPageComponent了
-            navigator.pop();
-        }
-    },
-    render: function() {
-      return (
-        <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-            <TouchableOpacity onPress={this._pressButton}>
-                <Text>点我跳回去</Text>
-            </TouchableOpacity>
-        </View>
-      )
-    }
-});
-
-var FirstPageComponent = React.createClass({
-    getInitialState: function() {
-        return {};
-    },
-    componentDidMount: function() {
-    },
-    _pressButton: function() {
-        const { navigator } = this.props;
-        //或者写成 const navigator = this.props.navigator;
-        //为什么这里可以取得 props.navigator?请看上文:
-        //<Component {...route.params} navigator={navigator} />
-        //这里传递了navigator作为props
-        if(navigator) {
-            navigator.push({
-                name: 'SecondPageComponent',
-                component: SecondPageComponent,
-            })
-        }
-    },
-    render: function() {
-        return (
-            <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-                <TouchableOpacity onPress={this._pressButton}>
-                    <Text>点我跳转 + {this.props.NavigationBar}</Text>
-                </TouchableOpacity>
-            </View>
-        );
-    }
-});
-
 const RootComponent = React.createClass({
   getInitialState(){
     this.props.onFetchQuests();
     return {};
   },
   render: function() {
-      var defaultName = 'FirstPageComponent';
-      var defaultComponent = FirstPageComponent;
+    const routeMapper = {
+      LeftButton: function(route, navigator, index, navState) {
+        if (index === 0) {
+          return null;
+        }
+        var previousRoute = navState.routeStack[index];
+        return (
+          <TouchableOpacity
+            onPress={() => navigator.pop()}>
+            <Text>
+              {previousRoute.title}
+            </Text>
+          </TouchableOpacity>
+        );
+      },
+
+      RightButton: function(route, navigator, index, navState) {
+        return (
+          <TouchableOpacity
+            onPress={() => navigator.push(newRandomRoute())}>
+            <Text>
+              下一个?
+            </Text>
+          </TouchableOpacity>
+        );
+      },
+
+      Title: function(route, navigator, index, navState) {
+        return (
+          <Text>
+            {route.name} [{index}]
+          </Text>
+        );
+      },
+    };// end of routeMapper
+      var defaultName = 'today';
+      var defaultComponent = TodayContainer;
+      var drawerStyles = {
+        backgroundColor: '#FFFFFF',
+        borderColor: '#000000',
+        borderWidth: 1,
+        // shadowOpacity: 0.8,
+        // shadowRadius: 3,
+        marginTop: 20
+      };
+      var mainStyles = {};
       return (
-        <Navigator
-          initialRoute={{ name: defaultName, component: defaultComponent }}
-          configureScene={() => {
-            return Navigator.SceneConfigs.VerticalDownSwipeJump;
-          }}
-          navigationBar={
-            <Navigator.NavigationBar
-              routeMapper={{
 
-                LeftButton: function(route, navigator, index, navState) {
-                  if (index === 0) {
-                    // return null;
-                  }
-
-                  var previousRoute = navState.routeStack[index];
-                  return (
-                    <TouchableOpacity
-                      onPress={() => navigator.pop()}
-                      style={styles.navBarLeftButton}>
-                      <Text style={[styles.navBarText, styles.navBarButtonText]}>
-                        {previousRoute.title + "<fuck"}
-                      </Text>
-                    </TouchableOpacity>
-                  );
-                },
-
-                RightButton: function(route, navigator, index, navState) {
-                  return (
-                    <TouchableOpacity
-                      onPress={() => navigator.push(newRandomRoute())}
-                      style={styles.navBarRightButton}>
-                      <Text style={[styles.navBarText, styles.navBarButtonText]}>
-                        Next
-                      </Text>
-                    </TouchableOpacity>
-                  );
-                },
-
-                Title: function(route, navigator, index, navState) {
-                  return (
-                    <Text style={[styles.navBarText, styles.navBarTitleText]}>
-                      {route.title} [{index}]
-                    </Text>
-                  );
-                },
-
-              }}
-              style={styles.navBar}
-            />
+        <Drawer
+          type="overlay"
+          content={
+            <Text>我是控制面板</Text>
           }
-          renderScene={(route, navigator) => {
-            let Component = route.component;
-            if(route.component) {
-              return <Component
-                {...route.params}
-                navigator={navigator} />
+          tapToClose={true}
+          openDrawerOffset={0.5}
+          panCloseMask={0.5}
+          closedDrawerOffset={-3}
+          styles={{
+            drawer: drawerStyles,
+            main: mainStyles
+          }}
+          tweenHandler={(ratio) => ({
+            main: { opacity:(2-ratio)/2 }
+          })}
+          >
+
+          <Navigator
+            style={{paddingLeft: 0, paddingRight: 0}}
+            initialRoute={{ name: defaultName, component: defaultComponent }}
+            configureScene={() => {
+              return Navigator.SceneConfigs.VerticalDownSwipeJump;
+            }}
+            navigationBar={
+              <Navigator.NavigationBar
+                style={{backgroundColor: "#01BAD2"}}
+                routeMapper={routeMapper}
+                />
             }
-          }} />
+            renderScene={(route, navigator) => {
+              let Component = route.component;
+              if(route.component) {
+                return <Component
+                  {...route.params}
+                  navigator={navigator} />
+              }
+            }} />
 
-      )
-    return (
-      <View style={styles.container}>
-        <ScrollView contentContainerStyle={styles.contentContainer}>
-
-          <View>
-              {this.props.quests.map( (quest) => {
-                return <Text key={quest.id} style={styles.welcome}>{quest.text}</Text>
-              })}
-          </View>
-        </ScrollView>
-        <ScrollView>
-          <ColoredRaisedButton/>
-          <Icon
-            name='ion|beer'
-            size={150}
-            color='#887700'
-            style={{width:100,height:100}}
-            />
-          <Icon
-            name='zocial|github'
-            size={70}
-            color='black'
-            style={{width:100,height:100}}
-            />
-          <Icon
-            name='fontawesome|facebook-square'
-            size={70}
-            color='#3b5998'
-            style={{width:100,height:100}}
-            />
-          <Icon
-            name='foundation|lightbulb'
-            size={30}
-            color='#777777'
-            style={{width:100,height:100}}
-            />
-
-          <Icon
-            name='material|face'
-            size={30}
-            color='#333333'
-            style={{width:100,height:100}}
-            />
-        </ScrollView>
-      </View>
-    );
+          </Drawer>
+        );
   }
 });
 
@@ -204,71 +117,6 @@ var styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  contentContainer: {
-    paddingVertical: 20
-  },
-  info: {
-    flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    padding: 3,
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-var styles = StyleSheet.create({
-  messageText: {
-    fontSize: 17,
-    fontWeight: '500',
-    padding: 15,
-    marginTop: 50,
-    marginLeft: 15,
-  },
-  button: {
-    backgroundColor: 'white',
-    padding: 15,
-    borderBottomColor: '#CDCDCD',
-  },
-  buttonText: {
-    fontSize: 17,
-    fontWeight: '500',
-  },
-  navBar: {
-    backgroundColor: 'white',
-  },
-  navBarText: {
-    fontSize: 16,
-    marginVertical: 10,
-  },
-  navBarTitleText: {
-    color: '#000000',
-    fontWeight: '500',
-    marginVertical: 9,
-  },
-  navBarLeftButton: {
-    paddingLeft: 10,
-  },
-  navBarRightButton: {
-    paddingRight: 10,
-  },
-  navBarButtonText: {
-    color: '#000000',
-  },
-  scene: {
-    flex: 1,
-    paddingTop: 20,
-    backgroundColor: '#EAEAEA',
   },
 });
 
@@ -284,4 +132,4 @@ function mapDispatchToProps(dispatch) {
   }
 }
 
-  module.exports = connect(mapStateToProps, mapDispatchToProps)(RootComponent)
+module.exports = connect(mapStateToProps, mapDispatchToProps)(RootComponent)
--- app/containers/TodayContainer.jsx
+++ app/containers/TodayContainer.jsx
@@ -1,31 +1,121 @@
-import React, { PropTypes } from 'react'
+import React from 'react-native';
 import {connect} from 'react-redux';
 import * as actions from '../actions/actionsCreators.jsx';
-import DataGridComponent from '../components/DataGridComponent.jsx'
+// import DataGridComponent from '../components/DataGridComponent.jsx'
+import MaterialKit from 'react-native-material-kit';
+import {Icon} from 'react-native-icons/index.ios';
+
+const {
+  ListView,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity
+} = React;
+
+const {
+  MKIconToggle,
+  MKCardStyles,
+  MKButton
+} = MaterialKit;
+
+const ColoredRaisedButton = MKButton.coloredButton()
+.withText('BUTTON')
+.withOnPress(() => {})
+.build();
 
 const TodayContainer = React.createClass({
-    render () {
-        return (
-            <div>
-                <DataGridComponent {...this.props} />
-            </div>
-        )
+    getInitialState: function() {
+      var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+      return {
+        dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+      };
+    },
+    componentDidMount: function() {
+    },
+    _pressButton: function() {
+        const { navigator } = this.props;
+        //或者写成 const navigator = this.props.navigator;
+        //为什么这里可以取得 props.navigator?请看上文:
+        //<Component {...route.params} navigator={navigator} />
+        //这里传递了navigator作为props
+        if(navigator) {
+            navigator.push({
+                name: 'TodayContainer',
+                component: TodayContainer,
+            })
+        }
+    },
+    handleRenderRow: function(rowData){
+      return (
+        <View style={{flexDirection: 'row', alignItems:"center", height: 60, borderWidth: 1}}>
+          <MKIconToggle
+            checked={false}
+            onCheckedChange={()=>{}}
+            onPress={()=>{}}
+            style={{width: 30,height: 30,borderWidth:1,borderRadius:500,margin:15}}
+            >
+            <Text
+              pointerEvents="none"
+              >卐</Text>
+            <Text
+              state_checked={true}
+              pointerEvents="none"
+              >卍</Text>
+          </MKIconToggle>
+          <Text style={{fontSize: 20}}>
+            {"\0\0" + rowData.text}
+          </Text>
+        </View>
+      )
+    },
+    render: function() {
+      return (
+        <View style={{flex:1, marginTop:60}}>
+          <ListView
+            dataSource={this.state.dataSource.cloneWithRows(this.props.quests)}
+            renderRow={this.handleRenderRow}
+          />
+
+          <TouchableOpacity onPress={this._pressButton}>
+            <MKButton
+              shadowRadius={2}
+              shadowOffset={{width:0, height:2}}
+              shadowOpacity={.7}
+              shadowColor="black"
+              onPress={this._pressButton}
+              >
+              <Text
+                pointerEvents="none"
+                style={{color: 'white', fontWeight: 'bold',}}>
+                我是 今日待办 点我跳转
+              </Text>
+            </MKButton>
+          </TouchableOpacity>
+        </View>
+      );
     }
-})
+});
+
+
+var styles = StyleSheet.create({
+  scene: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: '#EAEAEA',
+  },
+});
 
 function mapStateToProps(state) {
-    let quests = state.quests.filter((quest) => quest.type === 1 && quest.state === 0);
   return {
-      quests,
-      current: state.current
+      quests: state.quests
   }
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    onFetchEditQuest: (quest) => dispatch(actions.fetchEditQuest(quest)),
-    onFetchAddQuest: (text,type) => dispatch(actions.fetchAddQuest(text,type)),
-    onCompleteQuest: (id) => dispatch(actions.completeQuest(id))
+
   }
 }
 
